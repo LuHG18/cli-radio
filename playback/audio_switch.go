92d3079fa1cdd42aea81322d935cde5ac40242d1
Lucas Goldfarb <luhgoldfarb@gmail.com>
2025-05-20 23:27:36 -0500

    remove Blackhole device from stack when we leave the app so users default back to their typical devices

--- playback/audio_switch.go
+++ playback/audio_switch.go
@@ -1,6 +1,7 @@
 package playback
 
 import (
+	"bytes"
 	"fmt"
 	"os/exec"
 	"strings"
@@ -22,6 +23,28 @@ func getCurrentAudioDevice() (string, error) {
 		return "", fmt.Errorf("could not get current device: %w", err)
 	}
 	return strings.TrimSpace(string(currDevice)), nil
+}
+
+func getAvailableAudioDevices() ([]string, error) {
+	cmd := exec.Command("SwitchAudioSource", "-a", "-t", "output")
+
+	var out bytes.Buffer
+	cmd.Stdout = &out
+
+	if err := cmd.Run(); err != nil {
+		return nil, fmt.Errorf("failed to list audio devices: %w", err)
+	}
+
+	lines := strings.Split(out.String(), "\n")
+	var devices []string
+	for _, line := range lines {
+		trimmed := strings.TrimSpace(line)
+		if trimmed != "" {
+			devices = append(devices, trimmed)
+		}
+	}
+
+	return devices, nil
 }
 
 func switchAudioDevice(device string) error {
@@ -75,10 +98,36 @@ func SetupAudio() error {
 	return nil
 }
 
-// FIXME: might need to introduce some error handling here
-func RestoreAudio() {
-	if originalDevice != "" {
-		switchAudioDevice(originalDevice)
-		fmt.Printf("Restored to original audio device: %s\n", originalDevice)
+// contains checks whether the target string exists in the list of strings.
+func contains(list []string, target string) bool {
+	for _, item := range list {
+		if item == target {
+			return true
+		}
 	}
+	return false
+}
+
+func RestoreAudio() error {
+	current, _ := getCurrentAudioDevice()
+
+	if strings.Contains(current, "Blackhole") || strings.Contains(current, "Multi") {
+		available, _ := getAvailableAudioDevices()
+
+		// if the headphones are still availble (meaning we didn't turn them off during playback)
+		if contains(available, originalDevice) {
+			if err := switchAudioDevice(originalDevice); err != nil {
+				return err
+			}
+			// we want to remove the Blackhole output from the stack for when we turn off headphones at some point
+			// FIXME: need to make this more robust for other users (not hardcoded)
+			switchAudioDevice("MacBook Pro Speakers")
+			switchAudioDevice(originalDevice)
+		} else {
+			// 3. Fallback to MacBook speakers
+			switchAudioDevice("MacBook Pro Speakers")
+		}
+	}
+
+	return nil
 }
